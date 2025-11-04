ed10885007e003a249e88257a9ff3c57d2635d54
HaventlyGuard <tima.lyamin.05@mail.ru>
2025-11-04 22:29:34 +0700

    test lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Button, Input } from "@haventlyguard/lab6-package";
+
 export default function Home() {
   const buttonClick = () => {
     console.log("ok")
   }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
+        <Button variant="danger" size="md" type="button" onClick={()=>console.log("opa")} > дети</Button>
     </div>
   );
 }
